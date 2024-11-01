e4b41d7cbcc28951014b020eef21ab41ac6305e2
JoyllyHuynh <23130323@st.hcmuaf.edu.vn>
2024-11-01 15:23:53 +0700

    Add files via upload

---
+++ Lap5scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // JavaScript functionality for sorting, filtering, etc., can be added here
+    console.log("Page loaded. Add functionality as needed.");
+  });
+
